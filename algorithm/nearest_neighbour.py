d428e50df3f231ccb9e91b3a7142efc6c3807033
Giselle <gisellembrooks@gmail.com>
2018-11-24 12:55:50 +1000

    fixing errors

--- algorithm/nearest_neighbour.py
+++ algorithm/nearest_neighbour.py
@@ -9,14 +9,16 @@ import csv
 import random
 import math
 import operator
+import itertools 
+
  
 
 def loadDataset(filename, split, trainingSet=[] , testSet=[]):
-	with open(poi, 'rb') as csvfile:
+	with open('testdata.csv', 'r') as csvfile:
 	    lines = csv.reader(csvfile)
 	    dataset = list(lines)
 	    for x in range(len(dataset)-1):
-	        for y in range(4):
+	        for y in range(2):
 	            dataset[x][y] = float(dataset[x][y])
 	        if random.random() < split:
 	            trainingSet.append(dataset[x])
@@ -66,8 +68,8 @@ def main():
 	testSet=[]
 	split = 0.67
 	loadDataset('iris.data', split, trainingSet, testSet)
-	print 'Train set: ' + repr(len(trainingSet))
-	print 'Test set: ' + repr(len(testSet))
+	print ('Train set: ' + repr(len(trainingSet)))
+	print ('Test set: ' + repr(len(testSet)))
 	# generate predictions
 	predictions=[]
 	k = 3
