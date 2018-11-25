251e9db74db47af416838ee796e6bb2799bb8cea
Klein Geard <kleinmarkgeard@gmail.com>
2018-11-25 12:29:57 +1000

    added old pitch

---
+++ carto/cartointegration.py
@@ -0,0 +1,58 @@
+
+import argparse
+import logging
+import os
+import warnings
+
+from carto.auth import APIKeyAuthClient
+from carto.visualizations import VisualizationManager
+
+warnings.filterwarnings('ignore')
+
+# python export_map.py "results_1ertour_2012_1 map"
+
+# Logger (better than print)
+logging.basicConfig(
+    level=logging.INFO,
+    format=' %(asctime)s - %(levelname)s - %(message)s',
+    datefmt='%I:%M:%S %p')
+logger = logging.getLogger()
+
+# set input arguments
+parser = argparse.ArgumentParser(
+    description='Export a visualization')
+
+parser.add_argument('BoeingMissionControl', type=str,
+                    help='BoeingMissionControl')
+
+parser.add_argument('--Boeing', type=str, dest='Boeing',
+                    default=os.environ['CARTO_ORG'] if 'CARTO_ORG' in os.environ else '',
+                    help='Set the name of the organization' +
+                    ' account (defaults to env variable CARTO_ORG)')
+
+parser.add_argument('https://kleingeard.carto.com/builder/01823822-828d-4c4b-b51f-854b212eb0d4', type=str, dest='CARTO_BASE_URL',
+                    default=os.environ['CARTO_API_URL'] if 'CARTO_API_URL' in os.environ else '',
+                    help='Set the base URL. For example:' +
+                    ' https://username.carto.com/ ' +
+                    '(defaults to env variable CARTO_API_URL)')
+
+parser.add_argument('--api_key', dest='CARTO_API_KEY',
+                    default=os.environ['CARTO_API_KEY'] if 'CARTO_API_KEY' in os.environ else '',
+                    help='Api key of the account' +
+                    ' (defaults to env variable CARTO_API_KEY)')
+
+args = parser.parse_args()
+
+# Set authentification to CARTO
+if args.CARTO_BASE_URL and args.CARTO_API_KEY and args.organization:
+    auth_client = APIKeyAuthClient(
+        args.CARTO_BASE_URL, args.CARTO_API_KEY, args.organization)
+    visualization = VisualizationManager(auth_client).get(args.map_name)
+else:
+    logger.error('You need to provide valid credentials, run with -h parameter for details')
+    import sys
+    sys.exit(1)
+
+url = visualization.export()
+
+print('\nURL of .carto file is: ' + (url))
